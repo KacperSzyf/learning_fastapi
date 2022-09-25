3a1cd84ffa2da1e086a5dd31ccab8f1577d94d6e
Kacper <kacper@DESKTOP-EJB55QH.localdomain>
2022-09-25 17:05:42 +0100

    added studnet query by name

--- myapi.py
+++ myapi.py
@@ -19,6 +19,15 @@ students = {
 def index():
     return{"name": "test data"}
 
+#path parameters
 @app.get("/get-student/{student_id}")
 def get_student(student_id: int = Path(None, description="The ID of the student you would like to view:", gt = 0, lt = 3)):
     return students[student_id]
+
+#query parameter
+@app.get("/get-by-name")
+def get_Student(name: str):
+    for student_id in students:
+        if students[student_id]["name"] == name:
+            return students[student_id]
+    return ("Error": "Student not found!")
