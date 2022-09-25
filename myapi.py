524a8e77f35ed62d74e98fc46d9da2be0b555bdc
Kacper <kacper@DESKTOP-EJB55QH.localdomain>
2022-09-25 16:49:47 +0100

    added dummy data

--- myapi.py
+++ myapi.py
@@ -5,6 +5,15 @@ from fastapi import FastAPI
 #Main
 app = FastAPI()
 
+#Data
+students = {
+    1: {
+        "name": "jane",
+        "age": 20,
+        "class": 2022
+    }
+}
+
 #Endpoints
 @app.get("/")
 def index():
